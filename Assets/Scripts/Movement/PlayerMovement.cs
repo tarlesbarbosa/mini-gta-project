5044fdfb4e49b88a88ccffeaa144484fa22dce12
Tarles Barbosa <tarlesbarbosa@gmail.com>
2023-06-27 17:52:30 +0100

    Triggers done

--- Assets/Scripts/Movement/PlayerMovement.cs
+++ Assets/Scripts/Movement/PlayerMovement.cs
@@ -12,31 +12,48 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private Transform targetObject;
 
+    [SerializeField]
+    private bool isCharacterDead = false;
+
     // Update is called once per frame
     void Update()
     {
         float move = (Input.GetAxis("Vertical") * movementSpeed) * Time.deltaTime;
         float rotation = (Input.GetAxis("Horizontal") * rotationSpeed) * Time.deltaTime;
 
-        transform.Rotate(0, rotation, 0);
-
+        if(!isCharacterDead){
+            transform.Rotate(0, rotation, 0);
+        }
+        
         if(move != 0){
             characterAnimator.SetBool("isCharacterWalking", true);
         } else{
             characterAnimator.SetBool("isCharacterWalking", false);
         }
+
+        if(isCharacterDead && Input.GetKeyDown(KeyCode.Space)){
+            characterAnimator.SetTrigger("CharacterRevive");
+            isCharacterDead = false;
+        }
     }
 
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.CompareTag("Box")){
+            characterAnimator.SetTrigger("CharacterDeath");
+            isCharacterDead = true;
+        }
+    }
     // IK
 
-    private void OnAnimatorIK(int layerIndex)
-    {
-        characterAnimator.SetLookAtWeight(1);
-        characterAnimator.SetLookAtPosition(targetObject.position);
+    // private void OnAnimatorIK(int layerIndex)
+    // {
+    //     characterAnimator.SetLookAtWeight(1);
+    //     characterAnimator.SetLookAtPosition(targetObject.position);
 
-        characterAnimator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
-        characterAnimator.SetIKPosition(AvatarIKGoal.LeftHand, targetObject.position);
-    }
+    //     characterAnimator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+    //     characterAnimator.SetIKPosition(AvatarIKGoal.LeftHand, targetObject.position);
+    // }
 
     // END IK
 
